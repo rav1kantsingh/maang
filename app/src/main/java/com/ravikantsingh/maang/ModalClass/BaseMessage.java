ec68dc9a969398fc122e411b1bdb830974646b9b
iamravikantsingh <ravikantsingh2308@gmail.com>
2019-02-24 12:53:28 +0530

    hh

---
+++ app/src/main/java/com/ravikantsingh/maang/ModalClass/BaseMessage.java
@@ -0,0 +1,37 @@
+package com.ravikantsingh.maang.ModalClass;
+
+public class BaseMessage {
+    String message;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public User getSender() {
+        return sender;
+    }
+
+    public void setSender(User sender) {
+        this.sender = sender;
+    }
+
+    public long getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(long createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    User sender;
+    long createdAt;
+
+    class User {
+        String nickname;
+        String profileUrl;
+    }
+}
--- app/src/main/java/com/ravikantsingh/maang/NavDrawer/JantaDarbarActivity.java
+++ app/src/main/java/com/ravikantsingh/maang/NavDrawer/JantaDarbarActivity.java
@@ -6,10 +6,17 @@ import android.support.v7.app.AppCompatActivity;
 import com.ravikantsingh.maang.R;
 
 public class JantaDarbarActivity extends AppCompatActivity {
+//    private RecyclerView mMessageRecycler;
+//    private MessageListAdapter mMessageAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_janta_darbar);
+
+//        mMessageRecycler = (RecyclerView) findViewById(R.id.reyclerview_message_list);
+//        mMessageAdapter = new MessageListAdapter(this, messageList);
+//        mMessageRecycler.setLayoutManager(new LinearLayoutManager(this));
     }
+
 }
